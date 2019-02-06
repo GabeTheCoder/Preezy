6ff82f9a21f1b427a54433907b778b1418e21373
Gabe Pierannunzi <gabethecoder@icloud.com>
2019-02-05 19:41:54 -0500

    Initial Commit

--- Preezy/HomeViewController.swift
+++ Preezy/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Preezy
 //
 //  Created by Gabe The Coder on 2/5/19.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
 }
-
